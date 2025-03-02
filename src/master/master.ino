88a45a2e03e8ca7e608b731577eade8eb42c9918
Kien Deshpande <kiendeshpande@gmail.com>
2025-03-02 14:26:57 -0800

    functional and tested orientation code

--- src/master/master.ino
+++ src/master/master.ino
@@ -7,15 +7,61 @@
 #define BACKWARD 2
 #define LEFT     3
 #define RIGHT    4
-#define ROT_CW   5  // TO DO: implement rotation in drivetrain.cpp / .h
-#define ROT_CCW  6. // TO DO: implement rotation in drivetrain.cpp / .h
+#define ROT_CW   5  
+#define ROT_CCW  6  
 #define STOP     7
 
 #define SLAVE_ADDR 9 
 
-#define SERVO_PIN 8 
-#define US1_TRIG 6
-#define US1_ECHO 7
+
+// Servo pinouts
+#define SERVO_DROP_PIN 2 
+#define SERVO_FLAG_PIN 3
+
+// US pinouts: 4-13
+#define US_F_TRIG 5     // Front
+#define US_F_ECHO 4
+#define US_B_TRIG 3     // Back
+#define US_B_ECHO 2
+#define US_R_TRIG 7     // Right
+#define US_R_ECHO 6
+#define US_L1_TRIG 12   // Left 1
+#define US_L1_ECHO 13
+#define US_L2_TRIG 9    // Left 2
+#define US_L2_ECHO 8
+
+// IR Beacon pinout
+#define IR_PIN A0
+#define BEACON_THRESHOLD 3.3
+
+#define SZ_THRESH 18
+#define SZ_DIR 38
+
+
+/*---------------State Definitions--------------------------*/
+typedef enum {
+    STATE_START, STATE_ORIENT, STATE_FWD_1, STATE_RIGHT, STATE_FWD_2, 
+    STATE_INSIDE_LEFT, STATE_OUTSIDE_LEFT_1, STATE_INSIDE_BACK, 
+    STATE_OUTSIDE_BACK, STATE_OUTSIDE_LEFT_2, STATE_IGNITE, STATE_FWD, STATE_DROP
+  } States_t;
+  
+
+/*---------------Module Variables---------------------------*/
+States_t state;
+float us_f, us_b, us_r, us_l1, us_l2;
+uint16_t IR_value;
+
+bool command_sent = false; 
+
+// Initialize objects 
+//Payload p1(SERVO_DROP_PIN);
+//Payload p2(SERVO_FLAG_PIN);
+
+Ultrasonic us_front(US_F_TRIG, US_F_ECHO);
+Ultrasonic us_back(US_B_TRIG, US_B_ECHO);
+Ultrasonic us_right(US_R_TRIG, US_R_ECHO);
+Ultrasonic us_left1(US_L1_TRIG, US_L1_ECHO);
+Ultrasonic us_left2(US_L2_TRIG, US_L2_ECHO);
 
 // i2c TX sent as a struct with required data for one drivetrain command
 // this includes the task_id (forward, backward, etc.) and required arguments (speed and angle)
@@ -25,6 +71,8 @@ struct i2c_payload {
 };
 static i2c_payload tx_data; 
 
+
+/*---------------State and Helper Functions---------------------------*/
 
 /* Sends TX to slave given a drivetrain task id, speed, and angle. */
 void command_slave(uint8_t task_id, uint8_t speed = 100, uint8_t angle = 0) {
@@ -36,46 +84,90 @@ void command_slave(uint8_t task_id, uint8_t speed = 100, uint8_t angle = 0) {
     Wire.endTransmission();
 }
 
-// Initialize objects 
-Payload p1(SERVO_PIN);
-Ultrasonic us1(US1_TRIG, US1_ECHO);
 
 void setup() {
     Wire.begin();  // start i2c bus as master 
-    p1.begin();  // initialize payload 
-    us1.begin();  // initialize us1 
-
+    //p1.begin();  // initialize payload 
+    
+    // initialize ultrasonic 
+    // TO DO: write a sensors class and handle ultrasonics within this 
+    us_front.begin();  // initialize ultrasonic sensors
+    us_back.begin();
+    us_left1.begin();
+    us_left2.begin();
+    us_right.begin();
+  
     Serial.begin(9600);
+
+    state = STATE_ORIENT;
 }
 
+
+
 void loop() {
-    // simple test for ultrasonic sensor  
-    float us1_dist = us1.distance();
-    Serial.println("US1 distance in cm is:");
-    Serial.println(us1_dist); 
-    delay(1000);
+    checkGlobalEvents();
+    switch (state) {
+      case STATE_ORIENT:        
+        handleOrient();
+        break;
+      default:    
+        break;
+    }
+  }
 
-    // simple test servo release (open and close)
-    p1.release();
-    delay(1000);
 
-    // simple test sequence sending drivetrain commands to slave 
-    command_slave(FORWARD, 180); 
-    delay(3000);
-    command_slave(STOP); 
-    delay(2000);
-    command_slave(BACKWARD, 180); 
-    delay(3000);
-    command_slave(STOP); 
-    delay(2000);
-    command_slave(LEFT, 180); 
-    delay(3000);
-    command_slave(STOP); 
-    delay(2000);
-    command_slave(RIGHT, 180); 
-    delay(3000);
-    command_slave(STOP); 
-    delay(2000);
+// Handler for global events & responses
+void checkGlobalEvents(void) {
+    if (state == STATE_ORIENT) {
+        // ping all US sensors
+        us_f = us_front.distance();
+        us_b = us_back.distance();
+        us_r = us_right.distance();
+        us_l1 = us_left1.distance();
+        us_l2 = us_left2.distance();
+    } else if (state == STATE_RIGHT) {
+        // ping only right US sensor
+        // analog read beacon sensor
+        us_r = us_right.distance();
+        IR_value = analogRead(IR_PIN);
+        IR_value = map(IR_value, 0, 1023, 0, 5);
+    } else if ((state == STATE_INSIDE_LEFT) || 
+               (state == STATE_OUTSIDE_LEFT_1) || 
+               (state == STATE_OUTSIDE_LEFT_2)) {
+        // ping only left US sensor
+        // TODO: read both or just one of left US
+        us_l1 = us_left1.distance();
+    } else if ((state == STATE_FWD_1) || 
+               (state == STATE_FWD_2) || 
+               (state == STATE_INSIDE_BACK) || 
+               (state == STATE_OUTSIDE_BACK) || 
+               (state == STATE_DROP)) {
+        // ping only front US sensor
+        us_f = us_front.distance();
+    } else {
+        return;
+    }
+  }
+
+
+/* Routine for orientation state. Square the robot inside the start zone. CCW rotation until 
+the two leftside ultrasonics are aligned and the front ultrasonic faces desired direction */
+void handleOrient () {
+    float err = 0.4; // tolerance when determining alignment between us sensors 
+    bool direction = (us_f > SZ_DIR) & (us_l1 < SZ_THRESH) & (us_l2 < SZ_THRESH); 
+    bool aligned   = abs(us_l1 - us_l2) < err;  
+
+    if (direction & aligned) {
+        command_slave(STOP);
+        command_sent = false;
+        state = STATE_FWD;
+        return; // correctly oriented 
+    }
+
+    if (command_sent == false) {   // prevent repeated rotation commands 
+        command_slave(ROT_CCW, 90); 
+        command_sent = true; 
+    }
 }
 
 
