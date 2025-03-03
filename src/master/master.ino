0361dc4c1d2565e151291d32370f28cbbe21d23f
christopherlann <clann@stanford.edu>
2025-03-02 16:05:50 -0800

    Tested orient and fwd1

--- src/master/master.ino
+++ src/master/master.ino
@@ -3,12 +3,12 @@
 #include "payload.h"
 
 // Task IDs corresponding to drivetrain movement (used by I2C messaging)
-#define LEFT     1  // Chris changed Kien's numbering to reflect new naming convention
-#define RIGHT    2    
-#define BACKWARD 3
-#define FORWARD  4
-#define ROT_CW   5  // TO DO: implement rotation in drivetrain.cpp / .h
-#define ROT_CCW  6  // TO DO: implement rotation in drivetrain.cpp / .h
+#define FORWARD  1
+#define BACKWARD 2
+#define LEFT     3
+#define RIGHT    4      // TODO: change to reflect new forward frame of reference
+#define ROT_CW   5  
+#define ROT_CCW  6  
 #define STOP     7
 
 #define SLAVE_ADDR 9 
@@ -19,20 +19,19 @@
 #define SERVO_DROP_PIN 3
 
 // US pinouts: 4-13
-#define US_F_TRIG 4     // Front
-#define US_F_ECHO 5
+#define US_F_TRIG 5     // Front
+#define US_F_ECHO 4
+#define US_B_TRIG 3     // Back
+#define US_B_ECHO 2
+#define US_R_TRIG 7     // Right
+#define US_R_ECHO 6
+#define US_L1_TRIG 12   // Left 1
+#define US_L1_ECHO 13
+#define US_L2_TRIG 9    // Left 2
+#define US_L2_ECHO 8
 
-#define US_B_TRIG 6     // Back
-#define US_B_ECHO 7
-
-#define US_R_TRIG 8     // Right
-#define US_R_ECHO 9
-
-#define US_L1_TRIG 10   // Left 1
-#define US_L1_ECHO 11
-
-#define US_L2_TRIG 12   // Left 2
-#define US_L2_ECHO 13
+#define SZ_THRESH 18
+#define SZ_DIR 38
 
 // IR Beacon pinout
 #define IR_PIN A0
@@ -86,19 +85,11 @@ typedef enum {
   
 /*---------------Module Variables---------------------------*/
 States_t state;
-float us_front_dist, us_back_dist, us_right_dist, us_left1_dist, us_left2_dist;
+float us_f, us_b, us_r, us_l1, us_l2;   // Distance variables
 uint16_t IR_value;
 
-bool beacon_sensed = false;
-bool command_right_sent = false;
-bool command_fwd1_sent = false;
-bool command_fwd2_sent = false;
-bool command_inside_left_sent = false;
-bool command_outside_left1_sent = false;
-bool command_inside_back_sent = false;
-bool command_outside_back_sent = false;
-bool command_outside_left2_sent = false;
-bool command_fwd3_sent = false;
+bool beacon_sensed = false;     // beacon sensed flag
+bool command_sent = false;      // i2c command flag
 
 unsigned long startMillis;
 unsigned long currentMillis;
@@ -128,17 +119,18 @@ void setup() {
     us_left2.begin();
     us_right.begin();
     Serial.begin(9600);
-    state = STATE_START; // Initial state
+    // state = STATE_START; // Initial state
+    state = STATE_ORIENT;  
     startMillis = millis(); // Start time
 }
 
-
+// Test individual functions by commenting out cases to be left out
 void loop() {
     checkGlobalEvents();
     switch (state) {
-      case STATE_START:             // Start of round
-        handleStart();
-        break;
+    //   case STATE_START:             // Start of round
+    //     handleStart();
+    //     break;
       case STATE_ORIENT:            // Orientation
         handleOrient();
         break;
@@ -185,6 +177,7 @@ void loop() {
 
 
 // Handler for global events & responses
+// Poll sensors based off of current state
 void checkGlobalEvents(void) {
     currentMillis = millis();
     if (currentMillis - startMillis >= 130000) {    // 130 second rounds
@@ -192,15 +185,15 @@ void checkGlobalEvents(void) {
     }
     if (state == STATE_ORIENT) {
         // ping all US sensors
-        us_front_dist = us_front.distance();
-        us_back_dist = us_back.distance();
-        us_right_dist = us_right.distance();
-        us_left1_dist = us_left1.distance();
-        us_left2_dist = us_left2.distance();
+        us_f = us_front.distance();
+        us_b = us_back.distance();
+        us_r = us_right.distance();
+        us_l1= us_left1.distance();
+        us_l2 = us_left2.distance();
     } else if (state == STATE_RIGHT) {
         // ping only right US sensor
         // analog read beacon sensor
-        us_right_dist = us_right.distance();
+        us_r = us_right.distance();
         IR_value = analogRead(IR_PIN);
         IR_value = map(IR_value, 0, 1023, 0, 5);
     } else if ((state == STATE_INSIDE_LEFT) || 
@@ -208,16 +201,16 @@ void checkGlobalEvents(void) {
                (state == STATE_OUTSIDE_LEFT_2)) {
         // ping only left US sensor
         // TODO: read both or just one of left US
-        us_left1_dist = us_left1.distance();
+        us_l1 = us_left1.distance();
     } else if ((state == STATE_FWD_1) || 
                (state == STATE_FWD_2) || 
                (state == STATE_INSIDE_BACK) || 
                (state == STATE_OUTSIDE_BACK) || 
                (state == STATE_DROP)) {
         // ping only front US sensor
-        us_front_dist = us_front.distance();
+        us_f = us_front.distance();
     } else {
-        break;
+        return;
     }
   }
 
@@ -232,21 +225,36 @@ void handleStart(void) {
 }
 
 
-// Handler for orient state
-void handleOrient(void) {
-    // TODO: implement kien's code
+/* Routine for orientation state. Square the robot inside the start zone. CCW rotation until 
+the two leftside ultrasonics are aligned and the front ultrasonic faces desired direction */
+void handleOrient () {
+    float err = 0.4; // tolerance when determining alignment between us sensors 
+    bool direction = (us_f > SZ_DIR) & (us_l1 < SZ_THRESH) & (us_l2 < SZ_THRESH); 
+    bool aligned   = abs(us_l1 - us_l2) < err;  
+
+    if (direction & aligned) {
+        command_slave(STOP);
+        command_sent = false;
+        state = STATE_FWD_1;
+        return; // correctly oriented 
+    }
+
+    if (command_sent == false) {   // prevent repeated rotation commands 
+        command_slave(ROT_CCW, 90); 
+        command_sent = true; 
+    }
 }
 
 
 // Handler for when bot is moving fwd out of start zone 
 void handleFwd1(void) {
     // Move fwd
-    if (!command_fwd1_sent) {
-        command_slave(FORWARD, 180); 
-        command_fwd1_sent = true;
+    if (!command_sent) {
+        command_slave(LEFT, 120); 
+        command_sent = true;
     }
     // Check distance
-    if (us_front_dist < 13) {       // TODO: calibrate distance on course
+    if (us_f < 30) {       // TODO: calibrate distance on course
         state = STATE_RIGHT;
         command_slave(STOP);
     }
@@ -257,12 +265,12 @@ void handleFwd1(void) {
 // and searching for right wall or IR beacon
 void handleRight(void) {
     // Move right
-    if (!command_right_sent) {
+    if (!command_sent) {
         command_slave(RIGHT, 180); 
-        command_right_sent = true;
+        command_sent = true;
     }
     // Check distance
-    if (us_right_dist < 13) {       // TODO: calibrate distance on course
+    if (us_r < 13) {       // TODO: calibrate distance on course
         state = STATE_FWD_2;
         command_slave(STOP);
     } 
@@ -278,12 +286,12 @@ void handleRight(void) {
 // Handler for when bot is moving fwd to push pot
 void handleFwd2(void) {
     // Move forward
-    if (!command_fwd2_sent) {
+    if (!command_sent) {
         command_slave(FORWARD, 180); 
-        command_fwd2_sent = true;
+        command_sent = true;
     }
     // Check distance
-    if (us_front_dist < 7) {       // TODO: calibrate distance on course
+    if (us_f < 7) {       // TODO: calibrate distance on course
         if (beacon_sensed) {
             state = STATE_INSIDE_LEFT;
             beacon_sensed = false;
@@ -299,12 +307,12 @@ void handleFwd2(void) {
 // sensing left wall will be closer than when outside handles
 void handleInsideLeft(void) {
     // Move left
-    if (!command_inside_left_sent) {
+    if (!command_sent) {
         command_slave(LEFT, 180); 
-        command_inside_left_sent = true;
+        command_sent = true;
     }
     // Check distance
-    if (us_left1_dist < 7) {        // TODO: calibrate distance on course
+    if (us_l1 < 7) {        // TODO: calibrate distance on course
         state = STATE_INSIDE_BACK;
         command_slave(STOP);
     }
@@ -315,12 +323,12 @@ void handleInsideLeft(void) {
 // sensing left wall will be further than when inside handles
 void handleOutsideLeft1(void) {
     // Move left
-    if (!command_outside_left1_sent) {
+    if (!command_sent) {
         command_slave(LEFT, 180); 
-        command_outside_left1_sent = true;
+        command_sent = true;
     }
     // Check distance
-    if (us_left1_dist < 13) {       // TODO: calibrate distance on course
+    if (us_l1 < 13) {       // TODO: calibrate distance on course
         state = STATE_OUTSIDE_BACK;
         command_slave(STOP);
     }
@@ -330,12 +338,12 @@ void handleOutsideLeft1(void) {
 // Handler for when bot is backing out of handles
 void handleInsideBack(void) {
     // Move backwards
-    if (!command_inside_back_sent) {
+    if (!command_sent) {
         command_slave(BACKWARD, 180); 
-        command_inside_back_sent = true;
+        command_sent = true;
     }
     // Check distance
-    if (us_front_dist > 13) {        // TODO: calibrate distance on course
+    if (us_f > 13) {        // TODO: calibrate distance on course
         state = STATE_IGNITE;
         command_slave(STOP);
     }
@@ -345,12 +353,12 @@ void handleInsideBack(void) {
 // Handler for when bot is backing up when outside of handles
 void handleOutsideBack(void) {
     // Move backwards
-    if (!command_outside_back_sent) {
+    if (!command_sent) {
         command_slave(BACKWARD, 180); 
-        command_outside_back_sent = true;
+        command_sent = true;
     }
     // Check distance
-    if (us_front_dist > 13) {        // TODO: calibrate distance on course
+    if (us_f > 13) {        // TODO: calibrate distance on course
         state = STATE_OUTSIDE_LEFT_2;
         command_slave(STOP);
     }
@@ -360,12 +368,12 @@ void handleOutsideBack(void) {
 // Handler for when bot is going left to ignite
 void handleOutsideLeft2(void) {
     // Move left
-    if (!command_outside_left2_sent) {
+    if (!command_sent) {
         command_slave(LEFT, 180); 
-        command_outside_left2_sent = true;
+        command_sent = true;
     }
     // Check distance
-    if (us_left1_dist < 7) {       // TODO: calibrate distance on course
+    if (us_l1 < 7) {       // TODO: calibrate distance on course
         state = STATE_IGNITE;
         command_slave(STOP);
     }
@@ -377,18 +385,19 @@ void handleIgnite(void) {
     // TODO: Implement servo action
     // Blocking code is probably fine here
     p_ignite.release(); // TODO: configure servo to hit igniter
+    // TODO: add state transition
 } 
 
 
 // Handler for when bot is moving fwd to drop ball into pot
 void handleFwd3(void) {
     // Move forward
-    if (!command_fwd3_sent) {
+    if (!command_sent) {
         command_slave(FORWARD, 180); 
-        command_fwd3_sent = true;
+        command_sent = true;
     }
     // Check distance
-    if (us_front_dist < 7) {       // TODO: calibrate distance on course
+    if (us_f < 7) {       // TODO: calibrate distance on course
         state = STATE_DROP;
         command_slave(STOP);
     }
@@ -400,6 +409,7 @@ void handleDrop(void) {
     // TODO: Implement servo action
     // Blocking code is probably fine here
     p_drop.release(); // TODO: configure servo to release ingredient
+    // TODO: add state transition
 }
 
 
